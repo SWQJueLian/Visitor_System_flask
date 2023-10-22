ffa6928aa140ef7949c5904ab8f2b640f7d40c6a
绝恋。。 <1105033640@qq.com>
2023-10-22 11:37:10 +0800

    基础模型类默认继承SerializerMixin方便序列化输出

--- app/api/models.py
+++ app/api/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-
+from sqlalchemy_serializer import SerializerMixin
 from app import db
 
 
-class BaseModel(db.Model):
+class BaseModel(db.Model, SerializerMixin):
     __abstract__ = True  # 声明这是一个抽象的基类，不需要创建表
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow, default=datetime.utcnow)
